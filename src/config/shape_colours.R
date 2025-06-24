632f317f5a3f12e7c92ae626d9040bad83ddee1e
ASW-Analyst <andy.wilson8@nhs.net>
2025-06-24 15:13:48 +0100

    Add OS function shape colours config

---
+++ src/config/shape_colours.R
@@ -0,0 +1,17 @@
+
+# Define Palette for Greenspaces ------------------------------------------
+
+function_colors <- c(
+  "Allotments Or Community Growing Spaces" = "#FF6D24",
+  "Bowling Green" = "#B30202",
+  "Cemetery" = "#200266",
+  "Golf Course" = "#036480",
+  "Other Sports Facility" = "#FAFA00",
+  "Play Space" = "#5EB1FF",
+  "Playing Field" = "#A3F569",
+  "Public Park Or Garden" = "#00C21A",
+  "Religious Grounds" = "#FC7CBC",
+  "Tennis Court" = "#C8E8E5"
+)
+
+greenspace_types_list <- names(function_colors)
