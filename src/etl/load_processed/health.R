96adf239ec3287fcd593230abe0aee4b5574c333
ASW-Analyst <andy.wilson8@nhs.net>
2025-06-30 16:25:19 +0100

    Convert processed dfs to sf data frame

--- src/etl/load_processed/health.R
+++ src/etl/load_processed/health.R
@@ -4,7 +4,9 @@
 gp_practices_geo <- read.csv(paste0(here("data",
                                    "processed",
                                    "contextual",
-                                   "gp_practices_geo.csv")))
+                                   "gp_practices_geo.csv"))) |>
+  filter(!is.na(LAT)) |>
+  st_as_sf(coords = c("LON", "LAT"), crs = 4326, remove = FALSE)
 
 
 # Load Care Sites ---------------------------------------------------------
@@ -12,14 +14,18 @@ gp_practices_geo <- read.csv(paste0(here("data",
 care_sites_geo <- read.csv(paste0(here("data",
                                        "processed",
                                        "contextual",
-                                       "care_sites_geo.csv")))
+                                       "care_sites_geo.csv"))) |>
+  filter(!is.na(LAT)) |>
+  st_as_sf(coords = c("LON", "LAT"), crs = 4326, remove = FALSE)
 
 # Load Trust Sites --------------------------------------------------------
 
 trust_sites_geo <- read.csv(paste0(here("data",
                                         "processed",
                                         "contextual",
-                                        "trust_sites_geo.csv")))
+                                        "trust_sites_geo.csv"))) |>
+  filter(!is.na(LAT)) |>
+  st_as_sf(coords = c("LON", "LAT"), crs = 4326, remove = FALSE)
 
 
 # Load Pharmacies Locations -----------------------------------------------
@@ -27,4 +33,6 @@ trust_sites_geo <- read.csv(paste0(here("data",
 pharmacy_geo <- read.csv(paste0(here("data",
                                      "processed",
                                      "contextual",
-                                     "pharmacy_geo.csv")))
+                                     "pharmacy_geo.csv"))) |>
+  filter(!is.na(LAT)) |>
+  st_as_sf(coords = c("LON", "LAT"), crs = 4326, remove = FALSE)
